bd6bdf0cf0a6b5e5e1504e82648c99561337fc28
SybrenVanGastel <r0785524@student.thomasmore.be>
2021-10-15 11:17:42 +0200

    initial commit

---
+++ edge-service/src/test/java/be/itf/edgeservice/EdgeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package be.itf.edgeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
