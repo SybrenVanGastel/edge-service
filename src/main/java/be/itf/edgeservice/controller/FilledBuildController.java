43fea2785b0df80e3689e266dc0f5cda79463e5f
SybrenVanGastel <r0785524@student.thomasmore.be>
2021-12-10 11:40:01 +0100

    Testing

--- src/main/java/be/itf/edgeservice/controller/FilledBuildController.java
+++ src/main/java/be/itf/edgeservice/controller/FilledBuildController.java
@@ -32,15 +32,17 @@ public class FilledBuildController {
     @GetMapping("/builds")
     public ResponseEntity<Object> getBuilds() {
 //        try {
-            ResponseEntity<List<Build>> responseEntityBuilds =
-                    restTemplate.exchange(builderServiceBaseUrl + "/builders", HttpMethod.GET, null,
-                            new ParameterizedTypeReference<List<Build>>() {
-                            });
+        ResponseEntity<List<Build>> responseEntityBuilds =
+                restTemplate.exchange(builderServiceBaseUrl + "/builders", HttpMethod.GET, null,
+                        new ParameterizedTypeReference<List<Build>>() {
+                        });
 
-            List<Build> builds = responseEntityBuilds.getBody();
+        List<Build> builds = responseEntityBuilds.getBody();
 
-            assert builds != null;
-            return BuildResponseBuilder.generateBuildsOverview("Successfully returned all builds", HttpStatus.OK, builds);
+        System.out.println("Reached 1");
+        assert builds != null;
+        System.out.println("Reached 2");
+        return BuildResponseBuilder.generateBuildsOverview("Successfully returned all builds", HttpStatus.OK, builds);
 //        } catch (Exception e) {
 //            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 //        }
