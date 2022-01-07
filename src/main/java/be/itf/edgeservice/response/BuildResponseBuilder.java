42fd9bb9305a5b35dff9b3bf3bd4df66dc4a2efc
SybrenVanGastel <r0785524@student.thomasmore.be>
2022-01-07 12:51:39 +0100

    Fixed: sonar cloud code smells

--- src/main/java/be/itf/edgeservice/response/BuildResponseBuilder.java
+++ src/main/java/be/itf/edgeservice/response/BuildResponseBuilder.java
@@ -11,10 +11,27 @@ import java.util.Map;
 
 public class BuildResponseBuilder {
 
-    private final static String attributesStr = "attributes";
-    private final static String descriptionStr = "description";
-    private final static String imageUrlStr = "imageUrl";
-    private final static String isSelectedStr = "isSelected";
+    private static final String ID = "id";
+    private static final String NAME = "name";
+    private static final String USERNAME = "username";
+    private static final String TAG = "tag";
+    private static final String PRIMARY_WEAPON_NAME = "primaryWeaponName";
+    private static final String SECONDARY_WEAPON_NAME = "secondaryWeaponName";
+    private static final String PRIMARY_WEAPON = "primaryWeapon";
+    private static final String SECONDARY_WEAPON = "secondaryWeapon";
+    private static final String STRENGTH = "strength";
+    private static final String DEXTERITY = "dexterity";
+    private static final String FOCUS = "focus";
+    private static final String INTELLIGENCE = "intelligence";
+    private static final String CONSTITUTION = "constitution";
+    private static final String ATTRIBUTES = "attributes";
+    private static final String DESCRIPTION = "description";
+    private static final String IMAGE_URL = "imageUrl";
+    private static final String IS_SELECTED = "isSelected";
+    private static final String CATEGORY = "category";
+    private static final String COLOR = "color";
+    private static final String ABILITIES = "abilities";
+    private static final String SCALE_FACTOR = "scaleFactor";
 
     private BuildResponseBuilder() {
     }
@@ -26,12 +43,12 @@ public class BuildResponseBuilder {
         for (Build build : builds) {
             Map<String, Object> buildMap = new LinkedHashMap<>();
 
-            buildMap.put("id", build.getId());
-            buildMap.put("name", build.getName());
-            buildMap.put("username", build.getUsername());
-            buildMap.put("tag", build.getTag());
-            buildMap.put("primaryWeaponName", build.getPrimaryWeaponName());
-            buildMap.put("secondaryWeaponName", build.getSecondaryWeaponName());
+            buildMap.put(ID, build.getId());
+            buildMap.put(NAME, build.getName());
+            buildMap.put(USERNAME, build.getUsername());
+            buildMap.put(TAG, build.getTag());
+            buildMap.put(PRIMARY_WEAPON_NAME, build.getPrimaryWeaponName());
+            buildMap.put(SECONDARY_WEAPON_NAME, build.getSecondaryWeaponName());
 
             buildList.add(buildMap);
         }
@@ -43,100 +60,86 @@ public class BuildResponseBuilder {
         Map<String, Object> buildMap = new LinkedHashMap<>();
 
         // Basic information
-        buildMap.put("name", build.getName());
-        buildMap.put("username", build.getUsername());
-        buildMap.put("tag", build.getTag());
+        buildMap.put(NAME, build.getName());
+        buildMap.put(USERNAME, build.getUsername());
+        buildMap.put(TAG, build.getTag());
 
         // Attributes information
         Map<String, Object> attributesMap = new LinkedHashMap<>();
         List<Integer> attributes = build.getAttributeOptions();
 
-        attributesMap.put("strength", attributes.get(0));
-        attributesMap.put("dexterity", attributes.get(1));
-        attributesMap.put("intelligence", attributes.get(2));
-        attributesMap.put("focus", attributes.get(3));
-        attributesMap.put("constitution", attributes.get(4));
+        attributesMap.put(STRENGTH, attributes.get(0));
+        attributesMap.put(DEXTERITY, attributes.get(1));
+        attributesMap.put(INTELLIGENCE, attributes.get(2));
+        attributesMap.put(FOCUS, attributes.get(3));
+        attributesMap.put(CONSTITUTION, attributes.get(4));
 
-        buildMap.put(attributesStr, attributesMap);
+        buildMap.put(ATTRIBUTES, attributesMap);
 
         // Primary Weapon
         Map<String, Object> primaryWeaponMap = new LinkedHashMap<>();
         Weapon primaryWeapon = build.getPrimaryWeapon();
 
-        primaryWeaponMap.put("id", primaryWeapon.getId());
-        primaryWeaponMap.put("name", primaryWeapon.getName());
-        primaryWeaponMap.put(descriptionStr, primaryWeapon.getDescription());
-        primaryWeaponMap.put(imageUrlStr, primaryWeapon.getImageUrl());
+        primaryWeaponMap.put(ID, primaryWeapon.getId());
+        primaryWeaponMap.put(NAME, primaryWeapon.getName());
+        primaryWeaponMap.put(DESCRIPTION, primaryWeapon.getDescription());
+        primaryWeaponMap.put(IMAGE_URL, primaryWeapon.getImageUrl());
 
         List<Object> abilitiesPrimaryWeapon = new ArrayList<>();
         for (Ability ability : primaryWeapon.getAbilities()) {
-            Map<String, Object> abilityMap = new LinkedHashMap<>();
-
-            abilityMap.put("id", ability.getId());
-            abilityMap.put("name", ability.getName());
-            abilityMap.put(descriptionStr, ability.getDescription());
-            abilityMap.put(imageUrlStr, ability.getImageUrl());
-            abilityMap.put("category", ability.getCategory());
-            abilityMap.put("color", ability.getColor());
-            abilityMap.put(isSelectedStr, build.getSelectedAbilitiesWeapon1().contains(ability.getId()));
+            Map<String, Object> abilityMap = getAbilitiesFromWeapon(ability);
+            abilityMap.put(IS_SELECTED, build.getSelectedAbilitiesWeapon1().contains(ability.getId()));
 
             abilitiesPrimaryWeapon.add(abilityMap);
         }
-        primaryWeaponMap.put("abilities", abilitiesPrimaryWeapon);
+        primaryWeaponMap.put(ABILITIES, abilitiesPrimaryWeapon);
 
         List<Object> scaleWithAttributeList = new ArrayList<>();
         for (Attribute attribute : primaryWeapon.getAttributes()) {
             if (attribute.getScaleFactor() != 0) {
                 Map<String, Object> attributeMap = new LinkedHashMap<>();
 
-                attributeMap.put("name", attribute.getName());
-                attributeMap.put("scaleFactor", attribute.getScaleFactor());
+                attributeMap.put(NAME, attribute.getName());
+                attributeMap.put(SCALE_FACTOR, attribute.getScaleFactor());
 
                 scaleWithAttributeList.add(attributeMap);
             }
         }
-        primaryWeaponMap.put(attributesStr, scaleWithAttributeList);
+        primaryWeaponMap.put(ATTRIBUTES, scaleWithAttributeList);
 
-        buildMap.put("primaryWeapon", primaryWeaponMap);
+        buildMap.put(PRIMARY_WEAPON, primaryWeaponMap);
 
         // Secondary Weapon
         Map<String, Object> secondaryWeaponMap = new LinkedHashMap<>();
         Weapon secondaryWeapon = build.getSecondaryWeapon();
 
-        secondaryWeaponMap.put("id", secondaryWeapon.getId());
-        secondaryWeaponMap.put("name", secondaryWeapon.getName());
-        secondaryWeaponMap.put(descriptionStr, secondaryWeapon.getDescription());
-        secondaryWeaponMap.put(imageUrlStr, secondaryWeapon.getImageUrl());
+        secondaryWeaponMap.put(ID, secondaryWeapon.getId());
+        secondaryWeaponMap.put(NAME, secondaryWeapon.getName());
+        secondaryWeaponMap.put(DESCRIPTION, secondaryWeapon.getDescription());
+        secondaryWeaponMap.put(IMAGE_URL, secondaryWeapon.getImageUrl());
 
         List<Object> abilitiesSecondaryWeapon = new ArrayList<>();
         for (Ability ability : secondaryWeapon.getAbilities()) {
-            Map<String, Object> abilityMap = new LinkedHashMap<>();
-
-            abilityMap.put("id", ability.getId());
-            abilityMap.put("name", ability.getName());
-            abilityMap.put(descriptionStr, ability.getDescription());
-            abilityMap.put(imageUrlStr, ability.getImageUrl());
-            abilityMap.put("category", ability.getCategory());
-            abilityMap.put("color", ability.getColor());
-            abilityMap.put(isSelectedStr, build.getSelectedAbilitiesWeapon2().contains(ability.getId()));
+            Map<String, Object> abilityMap = getAbilitiesFromWeapon(ability);
+            abilityMap.put(IS_SELECTED, build.getSelectedAbilitiesWeapon2().contains(ability.getId()));
 
             abilitiesSecondaryWeapon.add(abilityMap);
         }
-        secondaryWeaponMap.put("abilities", abilitiesSecondaryWeapon);
+        secondaryWeaponMap.put(ABILITIES, abilitiesSecondaryWeapon);
 
         scaleWithAttributeList = new ArrayList<>();
         for (Attribute attribute : secondaryWeapon.getAttributes()) {
             if (attribute.getScaleFactor() != 0) {
                 Map<String, Object> attributeMap = new LinkedHashMap<>();
 
-                attributeMap.put("name", attribute.getName());
-                attributeMap.put("scaleFactor", attribute.getScaleFactor());
+                attributeMap.put(NAME, attribute.getName());
+                attributeMap.put(SCALE_FACTOR, attribute.getScaleFactor());
 
                 scaleWithAttributeList.add(attributeMap);
             }
         }
-        secondaryWeaponMap.put(attributesStr, scaleWithAttributeList);
-        buildMap.put("secondaryWeapon", secondaryWeaponMap);
+        secondaryWeaponMap.put(ATTRIBUTES, scaleWithAttributeList);
+        buildMap.put(SECONDARY_WEAPON, secondaryWeaponMap);
 
         return new ResponseEntity<>(buildMap, status);
     }
@@ -145,22 +148,14 @@ public class BuildResponseBuilder {
         Map<String, Object> weaponMap = new LinkedHashMap<>();
         List<Object> abilityList = new ArrayList<>();
 
-        weaponMap.put("id", weapon.getId());
-        weaponMap.put("name", weapon.getName());
+        weaponMap.put(ID, weapon.getId());
+        weaponMap.put(NAME, weapon.getName());
 
         for (Ability ability : weapon.getAbilities()) {
-            Map<String, Object> abilityMap = new LinkedHashMap<>();
-
-            abilityMap.put("id", ability.getId());
-            abilityMap.put("name", ability.getName());
-            abilityMap.put("description", ability.getDescription());
-            abilityMap.put("imageUrl", ability.getImageUrl());
-            abilityMap.put("category", ability.getCategory());
-            abilityMap.put("color", ability.getColor());
-
+            Map<String, Object> abilityMap = getAbilitiesFromWeapon(ability);
             abilityList.add(abilityMap);
         }
-        weaponMap.put("abilities", abilityList);
+        weaponMap.put(ABILITIES, abilityList);
 
         return new ResponseEntity<>(weaponMap, status);
     }
@@ -171,12 +166,25 @@ public class BuildResponseBuilder {
         for (Weapon weapon : weapons) {
             Map<String, Object> weaponMap = new LinkedHashMap<>();
 
-            weaponMap.put("id", weapon.getId());
-            weaponMap.put("name", weapon.getName());
+            weaponMap.put(ID, weapon.getId());
+            weaponMap.put(NAME, weapon.getName());
 
             weaponList.add(weaponMap);
         }
 
         return new ResponseEntity<>(weaponList, status);
+    }
+
+    private static Map<String, Object> getAbilitiesFromWeapon(Ability ability) {
+        Map<String, Object> abilityMap = new LinkedHashMap<>();
+
+        abilityMap.put(ID, ability.getId());
+        abilityMap.put(NAME, ability.getName());
+        abilityMap.put(DESCRIPTION, ability.getDescription());
+        abilityMap.put(IMAGE_URL, ability.getImageUrl());
+        abilityMap.put(CATEGORY, ability.getCategory());
+        abilityMap.put(COLOR, ability.getColor());
+
+        return abilityMap;
     }
 }
